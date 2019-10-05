5e731a2602cd8dbf60f04b78443a76898e418a7e
hinakouser <hinak406@gmail.com>
2019-10-05 19:05:11 +0530

    code commited by hina

---
+++ POM/category_page.py
@@ -0,0 +1,24 @@
+from Utilities import settings
+import logging
+from Utilities import test_base
+import time
+
+
+LOGGER = logging.getLogger(settings.LOG_NAME)
+
+
+def selecting_dresses(driver):
+
+	test_base.click(driver,"CP_btn_women")
+	test_base.click(driver,"CP_link_casual_dresses")
+	test_base.click(driver,"CP_chkbx_medium")
+	test_base.click(driver,"CP_img_picture")
+	test_base.click(driver,"CP_txtbx_qty")
+	test_base.click(driver,"CP_btn_add_to_cart")
+	test_base.click(driver,"CP_btn_proceed_chk_out_first_page")
+	test_base.click(driver,"CP_btn_proceed_chk_out_second_page")
+	test_base.click(driver,"CP_btn_proceed_chk_out_third_page")
+	test_base.click(driver,"CP_chkbx_terms_of_service")
+	test_base.click(driver,"CP_btn_proceed_chk_out_fourth_page")
+	test_base.click(driver,"CP_link_payment_option")
+	test_base.click(driver,"CP_btn_confirm_order")
--- test_execute.py
+++ test_execute.py
@@ -1,5 +1,6 @@
 import pytest
 from POM import login_page
+from POM import category_page
 from Utilities import xlsx,settings
 import logging
 
@@ -11,6 +12,11 @@ def get_test_data():
 	col_tc_desc = 2
 	col_tc_user_name = 3
 	col_tc_pwd = 4
+	col_tc_category = 5
+	col_tc_dresses = 6
+	col_tc_size = 7
+	col_tc_quantity = 8
+	col_tc_prize = 9
 	workbook = xlsx.load_excel("testdata.xlsx")
 	sheet_name = "Scenarios"
 	row_length = xlsx.get_row_length(workbook,sheet_name)
@@ -22,12 +28,22 @@ def get_test_data():
 		tc_desc = xlsx.read_cell(workbook,sheet_name,row,col_tc_desc)
 		tc_user_name = xlsx.read_cell(workbook,sheet_name,row,col_tc_user_name)
 		tc_pwd = xlsx.read_cell(workbook,sheet_name,row,col_tc_pwd)
+		tc_category = xlsx.read_cell(workbook,sheet_name,row,col_tc_category)
+		tc_dresses = xlsx.read_cell(workbook,sheet_name,row,col_tc_dresses)
+		tc_size = xlsx.read_cell(workbook,sheet_name,row,col_tc_size)
+		tc_quantity = xlsx.read_cell(workbook,sheet_name,row,col_tc_quantity)
+		tc_prize = xlsx.read_cell(workbook,sheet_name,row,col_tc_prize)
 
 		testdata_info.append({
 			"tc_id" : tc_id,
 			"tc_desc" : tc_desc,
 			"tc_user_name" : tc_user_name,
-			"tc_pwd" : tc_pwd
+			"tc_pwd" : tc_pwd,
+			"tc_category" : tc_category,
+			"tc_dresses" : tc_dresses,
+			"tc_size" : col_tc_size,
+			"tc_quantity" : tc_quantity,
+			"tc_prize" : tc_prize
 			#category
 			})
 	return testdata_info
@@ -38,5 +54,9 @@ def get_test_data():
 def test_login_to_automation_practice(driver,testdata):
 	LOGGER.info("%s"%(testdata))
 	assert login_page.login_your_logo(driver,testdata['tc_user_name'],testdata['tc_pwd']) is True
-    #write another function to select cateogory to accpt arguments
+	#write another function to select cateogory to accpt arguments
 
+@pytest.mark.parametrize("testdata",get_test_data())
+def test_select_the_dresses_from_automation_practice(driver,testdata):
+	LOGGER.info("%s"%(testdata))
+	category_page.selecting_dresses(driver)
